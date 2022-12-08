b4d55d74b326c8c8c818a5aef0069bce9cffdbdc
karthik <119947493+karthik76765@users.noreply.github.com>
2022-12-08 18:21:11 +0530

    Add files via upload

---
+++ values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("7/2=", 7/2)
+	fmt.Println("ture && false")
+	fmt.Println("ture %% false")
+	fmt.Println("!ture")
+
+}
